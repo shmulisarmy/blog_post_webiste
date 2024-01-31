4408218f9952f13d97023686ac2378de1736d018
shmulisarmy <shmulikeller@gmail.com>
2024-01-31 01:57:03 -0500

    added comment deleting

--- posts/urls.py
+++ posts/urls.py
@@ -5,5 +5,8 @@ from . import views
 urlpatterns = [
     path('make_post', views.make_post, name='make post'),
     path('home', views.homepage, name='home page'),
+    path('', views.homepage, name='home page'),
     path('view_post/<int:post_id>', views.view_post, name='view post'),
+    path('delete comment/<int:comment_id>', views.delete_comment, name='delete comment'),
+    path('delete post/<int:post_id>', views.delete_post, name='delete post'),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, reverse
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from .import  forms, models
 from django.http import HttpResponse
 # Create your views here.
@@ -25,7 +25,7 @@ def homepage(request):
     logged = request.session.get('logged')
     if logged:
         posts = models.Post.objects.filter(author=logged)  
-        return render(request, 'home.html', {'posts': posts})
+        return render(request, 'home.html', {'posts': posts, 'name': logged})
     else:
         return redirect(reverse('log in'))
     
@@ -44,3 +44,29 @@ def view_post(request, post_id):
     post =  models.Post.objects.filter(id=post_id).first()
     comments = models.Comment.objects.filter(on_post=post_id)
     return render(request, 'view_post.html', {'post': post, 'comments': comments, 'form': form})
+
+def delete_comment(request, comment_id):
+    logged = request.session.get('logged')
+    if request.method == 'POST':
+        post_id = request.POST.get('post_id')
+        comment = get_object_or_404(models.Comment, id=comment_id)
+        if comment.author !=  logged:
+            return HttpResponse("You do not have permission to edit this comment")
+        
+        comment.delete()
+        return view_post(request, post_id)
+        return redirect(reverse('view post', post_id=post_id))
+    else:
+        return HttpResponse('method must be delete')
+
+    
+
+def delete_post(request, post_id):
+    logged = request.session.get('logged')
+    if request.method == 'POST':
+        post = get_object_or_404(models.Post, id=post_id)
+        if post.author !=  logged:
+            return HttpResponse("You do not have permission to edit this post")
+        
+        post.delete()
+    return redirect(reverse('home page'))
